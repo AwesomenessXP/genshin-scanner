303dfb0a3a8bfcb5068a902315525bb45909ef89
HaskMoney <heartstring85@gmail.com>
2022-07-27 19:21:15 -0700

    deleted test file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require("path");
 module.exports = {
   entry: {
     index: "./src/index.js",
-    test: "./src/test.js",
     artifact_scan: "./src/artifact_scan.js",
   },
   module: {
