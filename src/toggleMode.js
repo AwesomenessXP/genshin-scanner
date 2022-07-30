5510f6b279a70d583f3ebe4b31dc69e20fe186eb
HaskMoney <heartstring85@gmail.com>
2022-07-30 01:58:50 -0700

    stuff

--- src/toggleMode.js
+++ src/toggleMode.js
@@ -1,8 +1,8 @@
 export function theme () {
-    const root = document.documentElement;
-    // change the mode in css
-    const setTheme = root.className === 'dark'
-        ? 'light'
-        : 'dark';
-    root.className = setTheme;
+	const root = document.documentElement;
+	// change the mode in css
+	const setTheme = root.className === 'dark'
+		? 'light'
+		: 'dark';
+	root.className = setTheme;
 }
