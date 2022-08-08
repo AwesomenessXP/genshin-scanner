1dc27e7e9b885bb64f2abdf1105c160493479c78
HaskMoney <heartstring85@gmail.com>
2022-08-07 22:31:57 -0700

    fixed bug where EM didnt show up correctly

--- src/artifacts/processText/regexStats.js
+++ src/artifacts/processText/regexStats.js
@@ -1,7 +1,7 @@
 export const regexStats = () => {
     const critRate = new RegExp("^CRIT Rate[+-]?([0-9]+\.?[0-9]*)\%$");
     const critDmg = new RegExp("^CRIT DMG[+-]?([0-9]+\.?[0-9]*)\%$");
-    const em = new RegExp("^Elemental Mastery[+-]?([0-9]+\.?[0-9]*)\%$");
+    const em = new RegExp("^Elemental Mastery[+-]?([0-9]*$");
     const flatATK = new RegExp("^ATK[+-]?[0-9]*$");
     const atkPcnt = new RegExp("^ATK[+-]?([0-9]+\.?[0-9]*)\%$");
     return {
